eb945323a61f591191a5a09f0268287191cbcdf5
M.A.Ahmed <ahmedanwer0094@gmail.com>
2024-11-02 19:36:14 +0530

    feat: update the gateway

--- backend/gateway/index.js
+++ backend/gateway/index.js
@@ -8,7 +8,10 @@ app.use(express.json());
 app.set("trust proxy", true);
 
 app.use("/users", proxy("https://user-639859377017.us-central1.run.app"));
-app.use("/shopping", proxy("http://localhost:8003"));
+app.use(
+  "/shopping",
+  proxy("https://shopping-639859377017.us-central1.run.app")
+);
 app.use("/", proxy("https://products-639859377017.us-central1.run.app"));
 
 app.all("*", async (req, res) => {
