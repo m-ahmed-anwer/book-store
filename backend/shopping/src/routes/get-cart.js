1885a9d046a51d342c0fb397c8ebd43bd803ee01
M.A.Ahmed <ahmedanwer0094@gmail.com>
2024-11-06 05:49:10 +0530

    feat:Update Changes

--- backend/shopping/src/routes/get-cart.js
+++ backend/shopping/src/routes/get-cart.js
@@ -4,7 +4,7 @@ import cart from "../model/cart.js";
 
 const router = express.Router();
 
-router.get("/", UserAuth, async (req, res, next) => {
+router.get("/get", UserAuth, async (req, res, next) => {
   const customerId = req.user.id;
 
   try {
